0553b4bed3f662a7a16be8a4a69c68b6d2654b45
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2024-08-30 10:46:02 -0300

    more advanced script

--- run.sh
+++ run.sh
@@ -32,28 +32,6 @@ $rc=[DCOps]::ReleaseDC([IntPtr]::Zero, $hdc)
 echo "$w,$h,$wm,$hm"
 '
 
-print_dpi_scale() {
-    local percent_str dpi_custom_percent dpi_scale_custom
-
-    percent_str=`python3 -c "print('%.0f' % round($1 * 100))"`
-
-    dpi_custom_percent=`python3 -c "print('%.6f' % round($dpi_100_percent * $1, 6))"`
-    dpi_scale_custom=`python3 -c "print('%.6f' % round($dpi_scale * $1, 6))"`
-
-    echo "#      (DPI-$percent_str%): ${dpi_custom_percent} DPI"
-    echo "#  - Scale set to: $dpi_scale_custom"
-    echo "# "
-    echo "export GDK_SCALE=${dpi_scale_custom}"
-    echo "export GDK_DPI_SCALE=${dpi_scale_custom}"
-    echo "export GTK_SCALE=${dpi_scale_custom}"
-    # https://doc.qt.io/qt-5/highdpi.html
-    # export QT_AUTO_SCREEN_SCALE_FACTOR=${QT_AUTO_SCREEN_SCALE_FACTOR:-1}
-    # export QT_ENABLE_HIGHDPI_SCALING=${QT_ENABLE_HIGHDPI_SCALING:-1}
-    echo "export QT_SCALE_FACTOR=${dpi_scale_custom}"
-    echo ""
-
-}
-
 dpi_scale_suggestion() {
 
     MainDisplayInfo=$("/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe" "$powershell_script")
@@ -110,20 +88,72 @@ dpi_scale_suggestion() {
     echo "#      (px): ${w}x${h}"
     echo "#      (mm): ${wm}x${hm}"
 
+    percent_str=`python3 -c "print('%.0f' % round($zoom * 100))"`
 
-    # echo "#      (DPI-100%): ${dpi_100_percent} DPI"
-    # echo "#  - Scale set to: $dpi_scale"
-    # echo "# "
-    # echo "export GDK_SCALE=${dpi_scale}"
-    # echo "export GDK_DPI_SCALE=${dpi_scale}"
-    # echo "export GTK_SCALE=${dpi_scale}"
-    # # https://doc.qt.io/qt-5/highdpi.html
-    # # export QT_AUTO_SCREEN_SCALE_FACTOR=${QT_AUTO_SCREEN_SCALE_FACTOR:-1}
-    # # export QT_ENABLE_HIGHDPI_SCALING=${QT_ENABLE_HIGHDPI_SCALING:-1}
-    # echo "export QT_SCALE_FACTOR=${dpi_scale}"
-    # echo ""
+    dpi_custom_percent=`python3 -c "print('%.6f' % round($dpi_100_percent * $zoom, 6))"`
+    dpi_scale_custom=`python3 -c "print('%.6f' % round($dpi_scale * $zoom, 6))"`
 
-    print_dpi_scale $zoom
+    echo "#      (DPI-$percent_str%): ${dpi_custom_percent} DPI"
+    echo "#  - Scale set to: $dpi_scale_custom"
+    echo "# "
+    echo "export GDK_SCALE=${dpi_scale_custom}"
+    echo "export GDK_DPI_SCALE=${dpi_scale_custom}"
+    echo "export GTK_SCALE=${dpi_scale_custom}"
+    # https://doc.qt.io/qt-5/highdpi.html
+    # export QT_AUTO_SCREEN_SCALE_FACTOR=${QT_AUTO_SCREEN_SCALE_FACTOR:-1}
+    # export QT_ENABLE_HIGHDPI_SCALING=${QT_ENABLE_HIGHDPI_SCALING:-1}
+    echo "export QT_SCALE_FACTOR=${dpi_scale_custom}"
+    echo ""
+
+while true; do
+    read -p "Do you want to add changes to ~./bashrc? (yes/no) " yn
+    case $yn in
+        [Yy]* ) 
+            echo ""
+            echo "creating ~/.wsl-hidpi-suggestion"
+            
+            echo "# wsl-hidpi-suggestion" > ~/.wsl-hidpi-suggestion
+            echo "# " >> ~/.wsl-hidpi-suggestion
+            echo "# Variable suggestion to use in the following files:" >> ~/.wsl-hidpi-suggestion
+            echo "#   ~/.bashrc" >> ~/.wsl-hidpi-suggestion
+            echo "#   /etc/profile" >> ~/.wsl-hidpi-suggestion
+            echo "# " >> ~/.wsl-hidpi-suggestion
+            echo "# DPI scale info:" >> ~/.wsl-hidpi-suggestion
+            echo "#  - Main size" >> ~/.wsl-hidpi-suggestion
+            echo "#      (px): ${w}x${h}" >> ~/.wsl-hidpi-suggestion
+            echo "#      (mm): ${wm}x${hm}" >> ~/.wsl-hidpi-suggestion
+            echo "#      (DPI-$percent_str%): ${dpi_custom_percent} DPI" >> ~/.wsl-hidpi-suggestion
+            echo "#  - Scale set to: $dpi_scale_custom" >> ~/.wsl-hidpi-suggestion
+            echo "# " >> ~/.wsl-hidpi-suggestion
+            echo "export GDK_SCALE=${dpi_scale_custom}" >> ~/.wsl-hidpi-suggestion
+            echo "export GDK_DPI_SCALE=${dpi_scale_custom}" >> ~/.wsl-hidpi-suggestion
+            echo "export GTK_SCALE=${dpi_scale_custom}" >> ~/.wsl-hidpi-suggestion
+            echo "export QT_SCALE_FACTOR=${dpi_scale_custom}" >> ~/.wsl-hidpi-suggestion
+
+
+            echo "adding '. ~/.wsl-hidpi-suggestion' to ~/.bashrc"
+            if `grep -Fxq ". ~/.wsl-hidpi-suggestion" ~/.bashrc`
+            then
+                echo " - entry already added to ~/.bashrc"
+            else
+                echo " - adding entry to ~/.bashrc"
+                echo ". ~/.wsl-hidpi-suggestion" >> ~/.bashrc
+            fi
+
+            break
+        ;;
+        [Nn]* ) 
+
+            break
+        ;;
+        * ) echo "Possible answer: yes or no.";;
+    esac
+done
+
+    export GDK_SCALE=${dpi_scale_custom}
+    export GDK_DPI_SCALE=${dpi_scale_custom}
+    export GTK_SCALE=${dpi_scale_custom}
+    export QT_SCALE_FACTOR=${dpi_scale_custom}
 
 }
 
